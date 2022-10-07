db68068036d2bda961f0bef5950ddb5baf7326a8
giangleee <giangle602@gmail.com>
2022-10-07 19:15:15 +0700

    update eslit

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="container">
     <section
-      class="section register min-vh-100 d-flex flex-column align-items-center justify-content-center py-4" >
+      class="section register min-vh-100 d-flex flex-column align-items-center justify-content-center py-4"
+    >
       <div class="container">
         <div class="row justify-content-center">
           <div
-            class="col-lg-4 col-md-6 d-flex flex-column align-items-center justify-content-center" >
+            class="col-lg-4 col-md-6 d-flex flex-column align-items-center justify-content-center"
+          >
             <div class="d-flex justify-content-center py-4">
               <a
                 href="index.html"
-                class="logo d-flex align-items-center w-auto" >
+                class="logo d-flex align-items-center w-auto"
+              >
                 <img src="assets/img/logo.png" alt="" />
                 <span class="d-none d-lg-block">Tên app</span>
               </a>
@@ -33,7 +36,9 @@
                       Username
                     </label>
                     <div class="input-group has-validation">
-                      <span class="input-group-text" id="inputGroupPrepend">@</span>
+                      <span class="input-group-text" id="inputGroupPrepend"
+                        >@</span
+                      >
                       <input
                         type="text"
                         name="username"
@@ -54,7 +59,7 @@
                       class="form-control"
                       id="yourPassword"
                       required
-                    /> 
+                    />
                   </div>
 
                   <div class="col-12">
@@ -83,8 +88,8 @@
                     data-button-type="continue_with"
                     data-layout="default"
                     data-auto-logout-link="false"
-                    data-use-continue-as="true">
-                  </div>
+                    data-use-continue-as="true"
+                  ></div>
                   <div class="col-12">
                     <p class="small mb-0">
                       Don't have account?
