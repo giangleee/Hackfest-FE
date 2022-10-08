03a4aea326003b24a0d37311badcfddc39efd705
giangleee <giangle602@gmail.com>
2022-10-08 13:24:28 +0700

    fix css

--- src/components/ReviKanjiInput.vue
+++ src/components/ReviKanjiInput.vue
@@ -1,13 +1,35 @@
 <template>
-     <div class="card col-md-6 revision-question">
-      <h3><strong>広告</strong></h3>
-      <div class="input-answer">
-        <label for="" class="text-left">Input your answer</label>
-        <br />
-        <input type="text" name="" id="" value="" class="form-control" />
-      </div>
-      <div class="">
-        <button type="submit" class="btn btn-primary">Submit</button>
-      </div>
+  <div class="card revision-question container container-fluid">
+    <h3><strong>広告</strong></h3>
+    <div class="input-answer">
+      <label
+        for=""
+        class="text-left"
+        >Input your answer</label
+      >
+      <br />
+      <input
+        type="text"
+        name=""
+        id=""
+        value=""
+        class="form-control" />
     </div>
+    <div class="">
+      <button
+        type="submit"
+        class="btn btn-primary">
+        Submit
+      </button>
+    </div>
+  </div>
 </template>
+
+<style lang="scss" scoped>
+.revision-question {
+  padding: 2rem;
+}
+.btn.btn-primary {
+  margin-top: 1.25rem;
+}
+</style>
